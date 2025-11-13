42612d863f7538fd83b0a96d7247e887e61b7101
Arkos Vähämäki <arkos@yousei.fi>
2025-11-13 05:44:04 +0000

    Create page-blogi.twig template and update page-blogi.php to match other page templates

--- page-blogi.php
+++ page-blogi.php
@@ -1,11 +1,21 @@
 <?php
 
+use Timber\Timber;
+
 $context = Timber::context();
+
+// Get posts for the blog archive
 $context['posts'] = Timber::get_posts([
     'post_type' => 'post',
     'posts_per_page' => 12,
     'paged' => get_query_var('paged') ? get_query_var('paged') : 1,
 ]);
 
-Timber::render('archive.twig', $context);
+// If there's a page with slug 'blogi', include it in context
+$page = Timber::get_post(['post_type' => 'page', 'name' => 'blogi']);
+if ($page) {
+    $context['post'] = $page;
+}
+
+Timber::render('page-blogi.twig', $context);
 
